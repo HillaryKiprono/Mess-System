b4f454a7219e9a6024c944bc9ab7fafaa0bc2f4e
HillaryKiprono <kipronohillary240@gmail.com>
2021-10-29 17:45:54 +0300

    second-commit

--- admin/SaveMenuItem.php
+++ admin/SaveMenuItem.php
@@ -7,33 +7,7 @@ if(isset($_POST['submit']))
     $fqty=$_POST['food_qty'];
     $fcode=$_POST['food_code'];
     $filename = 'image/'.$_FILES['image']['name'];
-    // Select file type
-//$imageFileType = strtolower(pathinfo($filename,PATHINFO_EXTENSION));
-	
-// valid file extensions
-//$extensions_arr = array("jpg","jpeg","png","gif");
-
-// Check extension
-//if( in_array($imageFileType,$extensions_arr) ){
-
-// Upload files and store in database
-
-//if(move_uploaded_file($_FILES["image"]["name"],'upload/'.$filename)){
-  // Image db insert sql
-  // $insert = "INSERT into image(file_name,uploaded_on,status) values('$filename',now(),1)";
-  //$sql="INSERT INTO fooditem(food_name,food_price,food_qty,food_code,image) 
-  //VALUES('$fname','$fprice','$fqty','$fcode','$filename')";
-  //if(mysqli_query($conn, $insert)){
-   // echo 'Data inserted successfully';
- // }
-  //else{
-   // echo 'Error: '.mysqli_error($conn);
-  //}
-// }else{
-//   echo 'Error in uploading file - '.$_FILES['image']['name'].'
-// ';
-// }   
-  $insert = "INSERT into image(file_name,uploaded_on,status) values('$filename',now(),1)";
+  
     $sql="INSERT INTO fooditem(food_name,food_price,food_qty,food_code,image) 
     VALUES('$fname','$fprice','$fqty','$fcode','$filename')";         
           if(mysqli_query($conn,$sql))
